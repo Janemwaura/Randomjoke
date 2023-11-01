fb341d48a685c5ef907f78c7d8e9b9584bc7655d
Janemwaura <mwaura461@gmail.com>
2023-11-01 14:15:39 +0300

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
-const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist&type=single";
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single";
 
 
 let getjoke = () => {
     fetch(url).then(data => data.json())
-    .then(item => console.log(item));
-}
+    .then(item => {
+        jokeContainer.textContent = `${item.joke}`;
+    });
+    }
+    btn.addEventListener("click",getjoke);
 getjoke ();
 getjoke ();
+getjoke();
