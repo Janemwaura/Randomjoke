1f2518d84a107a4ca570c309fb31389fdddd4531
Janemwaura <mwaura461@gmail.com>
2023-11-02 21:30:15 +0300

    changed btn to laugh Again

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single";
-
+const thumbsup = document.getElementById("thumbsup");
+const thumbsdown = document.getElementById("thumbsdown");
 
 let getjoke = () => {
     fetch(url).then(data => data.json())
