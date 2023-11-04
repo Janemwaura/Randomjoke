b734e0af797f4949bc016b47aeeed6aa84c2c518
Janemwaura <mwaura461@gmail.com>
2023-11-04 21:09:55 +0300

    actions to add event listeners on the Js file

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single";
-const thumbsup = document.getElementById("thumbsup");
-const thumbsdown = document.getElementById("thumbsdown");
+const thumbUp = document.querySelectorAll('.post-ratings')[0];
+const thumbDown = document.querySelectorAll('.post-ratings')[1];
 
+const thumbUpCount = thumbUp.querySelector('.post-rating-count');
+const thumbDownCount = thumbDown.querySelector('.post-rating-count');
+
+//event listener
+thumbUp.addEventListener('click', () => {
+    // Get the current count and increment it
+    let count = parseInt(thumbUpCount.textContent);
+    count++;
+    
+    // Update the count for thumb up
+    thumbUpCount.textContent = count;
+});
+
+// event listener 
+thumbDown.addEventListener('click', () => {
+    // Get the current count and increment it
+    let count = parseInt(thumbDownCount.textContent);
+    count++;
+    
+    // Update the count for thumb down
+    thumbDownCount.textContent = count;
+});
 let getjoke = () => {
     fetch(url).then(data => data.json())
     .then(item => {
@@ -14,3 +36,5 @@ let getjoke = () => {
 getjoke ();
 getjoke ();
 getjoke();
+
+
